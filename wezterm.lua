94f56d0a257dd7518e26b86f74eab46221be8db4
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-04-25 17:18:47 +0300

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+local config = {}
+
+config.color_scheme = 'Catppuccin Macchiato'
+
+config.font = wezterm.font('DejaVuSansMono NF')
+config.font_size = 14.0
+
+config.default_cwd = "e:/Workspace"
+
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
+
+return config
